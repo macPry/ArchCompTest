881fb9d3f4dac2ea50244574edbd6f1efb3f8179
macPry <maciej.przybyl@elpassion.pl>
2017-10-25 15:30:20 +0200

    Add activity

---
+++ app/src/main/java/pl/elpassion/archcomptest/details/DetailsActivity.kt
@@ -0,0 +1,14 @@
+package pl.elpassion.archcomptest.details
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pl.elpassion.archcomptest.R
+import kotlinx.android.synthetic.main.details_activity.*
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.details_activity)
+    }
+}
